93ea91b5c7dd277727113ccc04654836814436ea
LCL <1921699889@qq.com>
2025-12-12 00:42:38 +0800

    1

--- 第二层/main.py
+++ 第二层/main.py
@@ -40,13 +40,14 @@ class Run:
         self.wait_time=15
         temp=datetime.today()
         self.name=f"NEWG_{temp.year:04d}_{temp.month:02d}_{temp.day:02d}"
+        self.temp=None
 
     def init(self):
         conn=Connect_Nebula(self.config2)
         conn.open_nebula()
         client=conn.client
         try:
-            lt=client.execute("show spaces").as_data_frame()["Name"].values.tolist()
+            lt=client.execute("show spaces;").as_data_frame()["Name"].values.tolist()
             lt.sort()
             if len(lt)>14:
                 self.m.drop_space(client,lt[0])
@@ -114,6 +115,7 @@ class Run:
             df1.append([i[0],i[1],i[2],i[3],i[3],"接口",""])
         df=pd.DataFrame(df1,columns=["hostname","ip","brand","nic","mac_address","type","description"])
         self.create_nodes("nic",["hostname","nic"],df,chunk_size=10000)
+        self.temp=set([(i[0],i[3]) for i in df.values.tolist()])
 
     def create_edges(self,edge_type,relationship,chunk_size=1000):
         conn=Connect_Nebula(self.config2)
@@ -128,12 +130,38 @@ class Run:
 
     def create_between_network_and_interface_edges(self):
         db_mysql=Connect_Mysql(self.config1)
-        temp=db_mysql.get_table_data("","select * from topu.between_network_and_interface").values.tolist()
+        temp=db_mysql.get_table_data("","select * from topu.between_network_and_interface;").values.tolist()
         db_mysql.close()
         relationship1=[];relationship2=[]
         for i in temp:
-            relationship1.append([i[0],i[1],"网络"])
-            relationship2.append([i[1],i[0],"网络"])
+            relationship1.append([i[0],f"{i[0]}|{i[1]}","网络"])
+            relationship2.append([f"{i[0]}|{i[1]}",i[0],"网络"])
+        self.create_edges("network_to_interface",relationship1,chunk_size=10000)
+        self.create_edges("interface_to_network",relationship2,chunk_size=10000)
+
+    def create_between_interface_and_interface_edges(self):
+        db_mysql=Connect_Mysql(self.config1)
+        temp=db_mysql.get_table_data("","select lldpLocSysName,lldpRemSysName,lldpLocPortId,lldpRemPortId from topu.between_interface_and_interface;")[["lldpLocSysName","lldpRemSysName","lldpLocPortId","lldpRemPortId"]].values.tolist()
+        jh=set([(i[0],i[1]) for i in db_mysql.get_table_data("","select hostname,name from topu.interface")[["hostname","name"]].values.tolist()])
+        db_mysql.close()
+        relationship=[]
+        for i in temp:
+            if (i[0],i[2]) in jh and (i[1],i[3]) in jh:
+                relationship.append([f"{i[0]}|{i[2]}",f"{i[1]}|{i[3]}","网络"])
+                relationship.append([f"{i[1]}|{i[3]}",f"{i[0]}|{i[2]}","网络"])
+        self.create_edges("interface_to_interface",relationship,chunk_size=10000)
+
+    def create_between_interface_and_nic_edges(self):
+        db_mysql=Connect_Mysql(self.config1)
+        temp=db_mysql.get_table_data("","select server_hostname,nic,network_hostname,interface from topu.between_interface_and_nic;")[["server_hostname","nic","network_hostname","interface"]].values.tolist()
+        jh1=set([(i[0],i[1]) for i in db_mysql.get_table_data("","select hostname,name from topu.interface")[["hostname","name"]].values.tolist()])
+        jh2=self.temp
+        db_mysql.close()
+        relationship1=[];relationship2=[]
+        for i in temp:
+            if (i[0],i[1]) in jh2 and (i[2],i[3]) in jh21:
+                relationship1.append([i[0],i[1],"网络"])
+                relationship2.append([i[1],i[0],"网络"])
         self.create_edges("network_to_interface",relationship1,chunk_size=10000)
         self.create_edges("interface_to_network",relationship2,chunk_size=10000)
     
@@ -143,6 +171,8 @@ class Run:
         self.create_interface_nodes()
         self.create_nic_nodes()
         self.create_between_network_and_interface_edges()
+        self.create_between_interface_and_interface_edges()
+        self.create_between_interface_and_nic_edges()
 
 if __name__=="__main__":
     config1={
